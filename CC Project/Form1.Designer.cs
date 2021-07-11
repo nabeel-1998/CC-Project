d3df50c4ebfd5b3bdfb760322b8fd5d3dbb18bca
Asad Ullah <asadregards@gmail.com>
2021-07-11 14:58:15 +0500

    with symbol table

--- CC Project/Form1.Designer.cs
+++ CC Project/Form1.Designer.cs
@@ -29,43 +29,56 @@ namespace CC_Project
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.SourceCodeBox = new System.Windows.Forms.RichTextBox();
             this.TokenBox = new System.Windows.Forms.DataGridView();
-            this.ParsingTableButton = new System.Windows.Forms.Button();
             this.Lexeme = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Token = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ParsingTableButton = new System.Windows.Forms.Button();
+            this.SymbolTable = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TokenCodeBoc = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.TokenBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SymbolTable)).BeginInit();
             this.SuspendLayout();
             // 
             // SourceCodeBox
             // 
-            this.SourceCodeBox.Location = new System.Drawing.Point(12, 72);
+            this.SourceCodeBox.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SourceCodeBox.Location = new System.Drawing.Point(12, 36);
             this.SourceCodeBox.Name = "SourceCodeBox";
-            this.SourceCodeBox.Size = new System.Drawing.Size(421, 275);
+            this.SourceCodeBox.Size = new System.Drawing.Size(421, 209);
             this.SourceCodeBox.TabIndex = 1;
             this.SourceCodeBox.Text = "";
             this.SourceCodeBox.TextChanged += new System.EventHandler(this.SourceCodeBox_TextChanged);
             // 
             // TokenBox
             // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.TokenBox.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.TokenBox.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.TokenBox.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Lexeme,
             this.Token});
-            this.TokenBox.Location = new System.Drawing.Point(439, 72);
+            this.TokenBox.Location = new System.Drawing.Point(466, 69);
             this.TokenBox.Name = "TokenBox";
-            this.TokenBox.Size = new System.Drawing.Size(293, 366);
+            this.TokenBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TokenBox.Size = new System.Drawing.Size(293, 346);
             this.TokenBox.TabIndex = 2;
-            // 
-            // ParsingTableButton
-            // 
-            this.ParsingTableButton.Location = new System.Drawing.Point(46, 373);
-            this.ParsingTableButton.Name = "ParsingTableButton";
-            this.ParsingTableButton.Size = new System.Drawing.Size(182, 56);
-            this.ParsingTableButton.TabIndex = 4;
-            this.ParsingTableButton.Text = "Create Parsing Table";
-            this.ParsingTableButton.UseVisualStyleBackColor = true;
-            this.ParsingTableButton.Click += new System.EventHandler(this.ParsingTableButton_Click);
             // 
             // Lexeme
             // 
@@ -80,18 +93,133 @@ namespace CC_Project
             this.Token.ReadOnly = true;
             this.Token.Width = 150;
             // 
+            // ParsingTableButton
+            // 
+            this.ParsingTableButton.Location = new System.Drawing.Point(707, 447);
+            this.ParsingTableButton.Name = "ParsingTableButton";
+            this.ParsingTableButton.Size = new System.Drawing.Size(173, 49);
+            this.ParsingTableButton.TabIndex = 4;
+            this.ParsingTableButton.Text = "Create First and Follow Set ";
+            this.ParsingTableButton.UseVisualStyleBackColor = true;
+            this.ParsingTableButton.Click += new System.EventHandler(this.ParsingTableButton_Click);
+            // 
+            // SymbolTable
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.SymbolTable.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.SymbolTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SymbolTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Value,
+            this.Type});
+            this.SymbolTable.Location = new System.Drawing.Point(803, 69);
+            this.SymbolTable.Name = "SymbolTable";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.SymbolTable.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.SymbolTable.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.SymbolTable.Size = new System.Drawing.Size(262, 346);
+            this.SymbolTable.TabIndex = 5;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            this.Id.Width = 40;
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            this.Value.ReadOnly = true;
+            // 
+            // Type
+            // 
+            this.Type.HeaderText = "Type";
+            this.Type.Name = "Type";
+            this.Type.ReadOnly = true;
+            // 
+            // TokenCodeBoc
+            // 
+            this.TokenCodeBoc.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TokenCodeBoc.Location = new System.Drawing.Point(12, 287);
+            this.TokenCodeBoc.Name = "TokenCodeBoc";
+            this.TokenCodeBoc.Size = new System.Drawing.Size(421, 209);
+            this.TokenCodeBoc.TabIndex = 6;
+            this.TokenCodeBoc.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 21);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Source Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(228, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Source Code in token format";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(462, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 21);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Lexemes and Tokens";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(799, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 21);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Symbol Table";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1098, 508);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TokenCodeBoc);
+            this.Controls.Add(this.SymbolTable);
             this.Controls.Add(this.ParsingTableButton);
             this.Controls.Add(this.TokenBox);
             this.Controls.Add(this.SourceCodeBox);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.TokenBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SymbolTable)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -102,6 +230,15 @@ namespace CC_Project
         private System.Windows.Forms.Button ParsingTableButton;
         private System.Windows.Forms.DataGridViewTextBoxColumn Lexeme;
         private System.Windows.Forms.DataGridViewTextBoxColumn Token;
+        private System.Windows.Forms.DataGridView SymbolTable;
+        private System.Windows.Forms.RichTextBox TokenCodeBoc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Type;
     }
 }
 
